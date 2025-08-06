06345b8edbbfdcb2b5fdc72d12d4648e84f65868
Tim Meusel <tim@bastelfreak.de>
2025-08-06 22:20:09 +0200

    CI: Fix clojars.org credentials

--- project.clj
+++ project.clj
@@ -47,8 +47,8 @@
                  [io.github.clj-kondo/config-slingshot-slingshot "1.0.0"]]
 
   :deploy-repositories [["releases" {:url "https://clojars.org/repo"
-                                     :username :env/clojars_jenkins_username
-                                     :password :env/clojars_jenkins_password
+                                     :username :env/CLOJARS_USERNAME
+                                     :password :env/CLOJARS_PASSWORD
                                      :sign-releases false}]]
 
   ;; Convenience for manually testing application shutdown support - run `lein test-external-shutdown`
