8d40f311840025d1a9daca911ac78f7810d90ccd
OpenVoxProjectBot <215568489+OpenVoxProjectBot@users.noreply.github.com>
2025-12-31 21:26:01 +0000

    Version 4.1.3-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.openvoxproject/trapperkeeper "4.1.2"
+(defproject org.openvoxproject/trapperkeeper "4.1.3-SNAPSHOT"
   :description "A framework for configuring, composing, and running Clojure services."
 
   :license {:name "Apache License, Version 2.0"
